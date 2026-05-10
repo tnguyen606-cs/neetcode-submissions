6e7c577a98aaee1983e63b275f18a92b535e382f
Tam Nguyen <66191003+tnguyen606-cs@users.noreply.github.com>
2026-05-10 18:55:05 -0400

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        """
+        time: O(n)
+        space: O(1)
+        """
+        k = 0
+        for num in nums:
+            if num != val:
+                nums[k] = num
+                k += 1
+
+        return k
