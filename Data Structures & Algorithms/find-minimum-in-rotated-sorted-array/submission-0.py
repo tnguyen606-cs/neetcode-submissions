b24bfd5c97d5910c8d8e0d7ad66828c5f8a9150e
Tam Nguyen <66191003+tnguyen606-cs@users.noreply.github.com>
2026-08-17 14:47:20 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+
+        """
+
+        Institution:
+            Array Rotation: 
+
+        Given:
+            sorted in ascending order
+            rotated between 1 and n times
+            rotates
+            1 <= nums.length
+
+        Result:
+            the smallest element of the array
+
+        Approach: Binary search 
+            The smallest is where the cliff occurrs
+
+
+        Time: O(logn)
+        Space: O(1)
+
+        """
+
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            m = (l + r) // 2
+
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+        
+        return nums[l]
